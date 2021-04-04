6d5333be96e8526b5b2421a8e7500a579a1947c0
joaomsfgpires@gmail.com <joaomsfgpires@gmail.com">
2021-04-04 18:13:51 +0100

    godgamer nr 1

--- BackgammonClone/src/backgammonclone/BackgammonClone.java
+++ BackgammonClone/src/backgammonclone/BackgammonClone.java
@@ -16,6 +16,7 @@ public class BackgammonClone {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("gg wp trash top");
     }
     
 }
